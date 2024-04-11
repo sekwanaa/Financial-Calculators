d158bb2829d16a7353056594a4526e35ef78da04
Sekwang Chia <sk123rulz@gmail.com>
2024-04-11 14:15:03 -0400

    Finished Financial Calculators app workshop week 1

---
+++ src/main/java/com/pluralsight/FinancialCalculators.java
@@ -0,0 +1,63 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class FinancialCalculators {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Choose your calculator");
+            System.out.println("1. Mortgage");
+            System.out.println("2. Future Value of One-Time Deposit");
+            System.out.println("3. Present Value of Order Annuity");
+            int calculator = scanner.nextInt();
+
+            switch (calculator) {
+                case 1: //mortgage calculator
+                    System.out.println("Loan Amount: ");
+                    float loanAmount = scanner.nextFloat();
+                    System.out.println("Current Interest Rate per Year: ");
+                    float interestRate = scanner.nextFloat();
+                    System.out.println("Payment Length in Years: ");
+                    int loanLength = scanner.nextInt();
+
+                    int n = loanLength * 12;
+                    float i = (interestRate / 100) / 12;
+                    double monthlyPayment = (loanAmount * (i * Math.pow((1 + i), n))) / (Math.pow((1 + i), n) - 1);
+                    double totalInterest = (monthlyPayment * n) - loanAmount;
+
+                    System.out.printf("\nYour monthly loan payment is $%.2f with a total interest of $%.2f", monthlyPayment, totalInterest);
+                    break;
+
+                case 2: //future value of one time deposit
+                    System.out.println("Initial Deposit Amount: ");
+                    float initialDeposit = scanner.nextFloat();
+                    System.out.println("Current Interest Rate: ");
+                    float dailyInterestRate = scanner.nextFloat();
+                    System.out.println("Deposit Length in Years: ");
+                    int depositLength = scanner.nextInt();
+
+                    float ir = (dailyInterestRate / 100) / 365;
+                    double totalValue = (initialDeposit * (Math.pow((1 + ir), 365*depositLength)));
+                    double totalInterestEarned = totalValue - initialDeposit;
+
+                    System.out.printf("\nYour deposit will be worth $%.2f after %d years with a total interest of $%.2f", totalValue, depositLength, totalInterestEarned);
+                    break;
+
+                case 3: //present value of order annuity
+                    System.out.println("Expected Monthly Payout: ");
+                    float expectedMonthlyPayout = scanner.nextFloat();
+                    System.out.println("Expected Interest Rate: ");
+                    float expectedInterestRate = scanner.nextFloat();
+                    System.out.println("Payment Length in Years: ");
+                    int paymentLength = scanner.nextInt();
+
+                    float yir = (expectedInterestRate / 100) / 12;
+                    double presentValue = (expectedMonthlyPayout * ((1 - (Math.pow((1 + yir), (-12*paymentLength)))) / yir));
+
+                    System.out.printf("\nTo fund an annuity that pays $%.2f monthly, for %d years and earns an expected interest rate of %%%.2f, you would need to invest $%.2f today.\n", expectedMonthlyPayout, paymentLength, expectedInterestRate, presentValue);
+                    break;
+            }
+        } catch (Exception e) {
+            System.out.println("Please select the number corresponding to the calculator you would like to use.");
+        }
+    }
+}
